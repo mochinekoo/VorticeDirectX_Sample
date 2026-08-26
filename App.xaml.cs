22ae87fff74900f7d2694d99fe69cd536156505e
mochinekoo <mochinekoo0113@gmail.com>
2026-08-26 20:20:26 +0900

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace VorticeDirectX_Sample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
